1db59f29835fdc894bda6ef20e6a96d9e0777350
Sandhya Sridhar <ssandhyarmk@gmail.com>
2024-07-02 19:51:27 +0530

    Create NthFibonacciNumber.py

---
+++ DynammingProgramming/Memoization/NthFibonacciNumber.py
@@ -0,0 +1,17 @@
+class Memoization:
+    def __init__(self):
+        self.n=0
+        self.memo={}
+    def memoization_fibonacci(self,n):
+        if n in self.memo.keys():
+            return self.memo[n]
+        if n<=2:
+            return 1
+        else:
+            self.memo[n] = self.memoization_fibonacci(n-1)+self.memoization_fibonacci(n-2)
+            return self.memo[n]
+
+
+mem = Memoization()
+
+print(mem.memoization_fibonacci(6))
